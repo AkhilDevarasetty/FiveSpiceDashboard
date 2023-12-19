82035e9bc9d204c0ddee63682bf9f977ae020160
AkhilDevarasetty <akhild428@gmail.com>
2023-12-19 15:25:21 -0800

    initialised app and started creating folder structure

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import {dashboardData} from '../data';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'five-spice-dashboard';
+  constructor(private _httpClient: HttpClient) {
+    // this._httpClient
+    //   .get('http://api.fivespiceindiangrocery.com/api/products/produce')
+    //   .subscribe((response) => {
+    //     console.log(response);
+    //   });
+    console.log(dashboardData);
+    
+  }
+}
---
+++ src/app/categories/herbs/herbs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-herbs',
+  standalone: true,
+  imports: [],
+  templateUrl: './herbs.component.html',
+  styleUrl: './herbs.component.css'
+})
+export class HerbsComponent {
+
+}
