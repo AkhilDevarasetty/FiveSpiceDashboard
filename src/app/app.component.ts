b4a1598f2863c294a182c9c99588a97bdff57713
AkhilDevarasetty <akhild428@gmail.com>
2023-12-19 15:55:06 -0800

    completed project folder structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import {dashboardData} from '../data';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HttpClientModule],
+  imports: [CommonModule, RouterOutlet, HttpClientModule, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
